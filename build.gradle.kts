b483bc062004a619a2afdfa573d424a2e44c794c
gyowoo1113 <yereube@naver.com>
2026-01-05 22:00:28 +0900

    Setting : subprojects 설정변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,26 +10,23 @@ repositories {
 }
 
 subprojects {
-    // 모든 모듈 공통: 자바 + 테스트 + 리포지토리
-    apply(plugin = "java")
+    repositories { mavenCentral() }
 
-    repositories {
-        mavenCentral()
-    }
-
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(21))
+    plugins.withType<JavaPlugin> {
+        extensions.configure<JavaPluginExtension> {
+            toolchain.languageVersion.set(JavaLanguageVersion.of(21))
         }
-    }
 
-    tasks.test {
-        useJUnitPlatform()
-    }
+        tasks.withType<Test> { useJUnitPlatform() }
 
-    dependencies {
-        // 공통 테스트 의존성
-        testImplementation(platform("org.junit:junit-bom:5.10.0"))
-        testImplementation("org.junit.jupiter:junit-jupiter")
+        dependencies {
+            "compileOnly"("org.projectlombok:lombok:1.18.30")
+            "annotationProcessor"("org.projectlombok:lombok:1.18.30")
+            "testCompileOnly"("org.projectlombok:lombok:1.18.30")
+            "testAnnotationProcessor"("org.projectlombok:lombok:1.18.30")
+
+            "testImplementation"(platform("org.junit:junit-bom:5.10.0"))
+            "testImplementation"("org.junit.jupiter:junit-jupiter")
+        }
     }
 }
