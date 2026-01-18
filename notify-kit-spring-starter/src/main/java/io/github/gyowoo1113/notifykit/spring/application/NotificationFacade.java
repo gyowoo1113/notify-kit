a354c843a3f869db1a93ce0f4c2cfa6c83d17e6d
gyowoo1113 <yereube@naver.com>
2026-01-18 12:10:17 +0900

    Feat #4 : facade create 시 알림 publish 하도록 추가, 커밋 후에 진행되도록 보완 필요

--- notify-kit-spring-starter/src/main/java/io/github/gyowoo1113/notifykit/spring/application/NotificationFacade.java
+++ notify-kit-spring-starter/src/main/java/io/github/gyowoo1113/notifykit/spring/application/NotificationFacade.java
@@ -7,6 +7,7 @@ import io.github.gyowoo1113.notifykit.core.domain.support.NotificationUpdate;
 import io.github.gyowoo1113.notifykit.core.service.NotificationService;
 import io.github.gyowoo1113.notifykit.core.support.CursorPage;
 import io.github.gyowoo1113.notifykit.core.support.PageResult;
+import io.github.gyowoo1113.notifykit.spring.infrastructure.delivery.event.NotificationEventPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,10 +16,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class NotificationFacade {
     private final NotificationService coreService;
+    private final NotificationEventPublisher eventPublisher;
 
     @Transactional
     public Notification create(NotificationCreate create) {
-        return coreService.create(create);
+        Notification saved = coreService.create(create);
+        eventPublisher.publishCreated(saved);
+        return saved;
     }
 
     @Transactional(readOnly = true)
