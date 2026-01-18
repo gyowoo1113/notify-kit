ef631290ea02e9120c39f48850ca90758795509c
gyowoo1113 <yereube@naver.com>
2026-01-18 12:09:33 +0900

    Feat #4 : sse 관련 web autoconfiguration 추가

--- notify-kit-spring-starter/src/main/java/io/github/gyowoo1113/notifykit/spring/config/NotifyWebAutoConfiguration.java
+++ notify-kit-spring-starter/src/main/java/io/github/gyowoo1113/notifykit/spring/config/NotifyWebAutoConfiguration.java
@@ -1,14 +1,46 @@
 package io.github.gyowoo1113.notifykit.spring.config;
 
 import io.github.gyowoo1113.notifykit.spring.infrastructure.delivery.advice.ExceptionControllerAdvice;
+import io.github.gyowoo1113.notifykit.spring.infrastructure.delivery.event.NoopNotificationEventPublisher;
+import io.github.gyowoo1113.notifykit.spring.infrastructure.delivery.event.NotificationEventPublisher;
+import io.github.gyowoo1113.notifykit.spring.infrastructure.delivery.sse.SseEmitterRegistry;
+import io.github.gyowoo1113.notifykit.spring.infrastructure.delivery.sse.SseNotificationEventPublisher;
+import io.github.gyowoo1113.notifykit.spring.infrastructure.delivery.sse.SseSubscribeController;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @AutoConfiguration
-@ConditionalOnClass(RestControllerAdvice.class)
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+@ConditionalOnClass(name = "org.springframework.web.servlet.DispatcherServlet")
 @Import(ExceptionControllerAdvice.class)
 public class NotifyWebAutoConfiguration {
 
+    @Bean
+    @ConditionalOnMissingBean
+    public SseEmitterRegistry sseEmitterRegistry(){
+        return new SseEmitterRegistry();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(NotificationEventPublisher.class)
+    public NotificationEventPublisher noopNotificationEventPublisher() {
+        return new NoopNotificationEventPublisher();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "notify.sse.enabled", havingValue = "true")
+    public NotificationEventPublisher sseNotificationEventPublisher(SseEmitterRegistry registry) {
+        return new SseNotificationEventPublisher(registry);
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "notify.sse.enabled", havingValue = "true")
+    public SseSubscribeController sseSubscribeController(SseEmitterRegistry registry) {
+        return new SseSubscribeController(registry);
+    }
 }
