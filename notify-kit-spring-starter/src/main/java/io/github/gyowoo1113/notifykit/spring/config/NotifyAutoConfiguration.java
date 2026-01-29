85aecbf7dee3809932bac1912993e7c421c513bd
gyowoo1113 <yereube@naver.com>
2026-01-29 09:37:03 +0900

    Feat #11 : add auto-configuration for outbox processor and worker

--- notify-kit-spring-starter/src/main/java/io/github/gyowoo1113/notifykit/spring/config/NotifyAutoConfiguration.java
+++ notify-kit-spring-starter/src/main/java/io/github/gyowoo1113/notifykit/spring/config/NotifyAutoConfiguration.java
@@ -2,12 +2,15 @@ package io.github.gyowoo1113.notifykit.spring.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.gyowoo1113.notifykit.core.service.NotificationService;
+import io.github.gyowoo1113.notifykit.core.service.OutboxProcessorService;
 import io.github.gyowoo1113.notifykit.core.service.port.EventIdGenerator;
 import io.github.gyowoo1113.notifykit.core.service.port.NotificationRepository;
 import io.github.gyowoo1113.notifykit.core.service.port.OutboxRepository;
+import io.github.gyowoo1113.notifykit.core.service.port.OutboxSender;
 import io.github.gyowoo1113.notifykit.spring.application.NotificationFacade;
 import io.github.gyowoo1113.notifykit.spring.infrastructure.delivery.common.AtomicEventIdGenerator;
 import io.github.gyowoo1113.notifykit.spring.infrastructure.delivery.event.NotificationCreatedEventListener;
+import io.github.gyowoo1113.notifykit.spring.infrastructure.delivery.outbox.OutboxWorker;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -39,7 +42,22 @@ public class NotifyAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
+    @ConditionalOnBean({OutboxRepository.class, OutboxSender.class})
     public NotificationCreatedEventListener notificationCreatedEventListener(OutboxRepository outboxRepository, ObjectMapper objectMapper, EventIdGenerator generator){
         return new NotificationCreatedEventListener(outboxRepository,objectMapper,generator);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnBean({OutboxRepository.class, OutboxSender.class})
+    public OutboxProcessorService outboxProcessorService(OutboxRepository repository, OutboxSender sender){
+        return new OutboxProcessorService(repository,sender);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnBean(OutboxProcessorService.class)
+    public OutboxWorker outboxWorker(OutboxProcessorService processorService){
+        return new OutboxWorker(processorService);
     }
 }
